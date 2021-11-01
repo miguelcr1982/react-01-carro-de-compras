6728da2419fa96771714bf5963b321de0f1c54b9
miguel <miguelcr1982@gmail.com>
2021-10-31 21:32:53 -0600

    Primer commit

---
+++ src/components/Producto.js
@@ -0,0 +1,34 @@
+import { Component } from "react";
+import Button from './Button'
+
+const styles = {
+    producto: {
+        border: 'solid 1px #eee',
+        boxShadow: '0 5px 5px rgb(0,0,0, 0.1)',
+        width: '30%',
+        padding: '10px 15px',
+        boderRadius: '5px'
+    },
+    img: {
+        width: 'auto',
+        height: '150px'
+    }
+}
+
+class Producto extends Component {
+    render() {
+        const { producto, agregarAlCarro } = this.props;
+
+        return (<div style={styles.producto}>
+            <img style={styles.img} alt={producto.name} src={producto.img} />
+            <h3>{producto.name}</h3>
+            <p>{producto.price}</p>
+
+            <Button onClick={() => agregarAlCarro(producto)}>
+                Agregar al carro
+            </Button>
+        </div>)
+    }
+}
+
+export default Producto;
